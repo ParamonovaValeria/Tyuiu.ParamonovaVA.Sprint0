5292092d266ed1e11b859cbe4313218d061be9a5
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-09-15 22:11:54 +0500

    Task1

---
+++ Tyuiu.ParamonovaVA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.ParamonovaVA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Валерия";
+            int age = 19;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + "  мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
