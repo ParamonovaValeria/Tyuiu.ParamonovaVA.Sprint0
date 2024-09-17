b6750b79a06ea01a50e6e0f5795b767f0121cea3
Tyuiu.ParamonovaVA@outlook.com <paramonova-05@mail.ru>
2024-09-17 20:53:26 +0500

    Task6

---
+++ Tyuiu.ParamonovaVA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ParamonovaVA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.ParamonovaVA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массивва = " + DataService.AdditionArray(numsArray));
+
+            Console.WriteLine("Разность элементов массивва = " + DataService.SubtractionArray(numsArray));
+
+            Console.WriteLine("Произведение элементов массивва = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
